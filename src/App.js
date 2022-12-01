d06d4ff5864dcfd02767639411a2c8925e122539
Joe Rehfuss <jrehfuss@c02g81atq05q.domain>
2022-12-01 10:45:22 -0800

    updates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Kitten from "./pages/Kitten";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/taco" element={<h1>taco page</h1>}/>
+          <Route path="/kitten/:id" element={<Kitten/>}/>
+          <Route path="*" element={<h1>404 page</h1>}/>
+        </Routes>
+        <h1>Footer</h1>
+      </Router>
     </div>
   );
 }
